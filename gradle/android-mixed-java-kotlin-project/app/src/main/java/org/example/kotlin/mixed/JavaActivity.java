c563184646054744598bfce033f9cdf243d30b2c
Malachi de AElfweald <malachid@gmail.com>
2016-03-04 10:01:56 -0800

    Databinding is technically working though the IDE is falsely reporting errors See also: https://youtrack.jetbrains.com/issue/KT-8007

--- gradle/android-mixed-java-kotlin-project/app/src/main/java/org/example/kotlin/mixed/JavaActivity.java
+++ gradle/android-mixed-java-kotlin-project/app/src/main/java/org/example/kotlin/mixed/JavaActivity.java
@@ -1,20 +1,26 @@
 package org.example.kotlin.mixed;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
-
+import org.example.kotlin.mixed.databinding.ActivityMainBinding;
 import org.example.kotlin.mixed.KotlinActivity;
 
-public class JavaActivity extends Activity {
+public class JavaActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.setDatabound(String.format("%s is databound", getClass().getSimpleName()));
+        binding.executePendingBindings();
 
         Button next = (Button) findViewById(R.id.Button01);
         next.setOnClickListener(new View.OnClickListener() {
--- gradle/android-mixed-java-kotlin-project/app/src/main/kotlin/org/example/kotlin/mixed/KotlinActivity.kt
+++ gradle/android-mixed-java-kotlin-project/app/src/main/kotlin/org/example/kotlin/mixed/KotlinActivity.kt
@@ -3,14 +3,21 @@ package org.example.kotlin.mixed
 import android.content.Intent
 import android.os.Bundle
 import android.app.Activity
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.widget.Button
+import org.example.kotlin.mixed.databinding.ActivityMain2Binding
 
-open class KotlinActivity : Activity() {
+open class KotlinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        val binding = DataBindingUtil.setContentView<ActivityMain2Binding>(this, R.layout.activity_main2)
+
+        // NOTE: Kotlin executes the next line correctly, but Android Studio shows it as an error
+        binding.setDatabound(String.format("%s is databound", javaClass.simpleName))
+        binding.executePendingBindings()
 
         var next = findViewById(R.id.Button02) as Button
         next.setOnClickListener {
